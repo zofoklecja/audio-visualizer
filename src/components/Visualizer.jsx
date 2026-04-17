0cdbcfeb34869c33f54e695615e97e13d2c661ff
Zofia Drozd <drozd.zosia@gmail.com>
2026-04-17 14:40:25 +0200

    feat: implement responsive canvas size

--- src/components/Visualizer.jsx
+++ src/components/Visualizer.jsx
@@ -1,12 +1,32 @@
 import { useEffect, useRef } from "react";
 
-const WIDTH = 600;
-const HEIGHT = 400;
+const DEFAULT_WIDTH = 600;
+const DEFAULT_HEIGHT = 400;
 
 function Visualizer({ stream, active }) {
 	const canvasRef = useRef(null);
+	const wrapperRef = useRef(null);
 	const activeRef = useRef(true);
 	const drawRef = useRef(null);
+
+	useEffect(() => {
+		if (!wrapperRef?.current) return;
+		const resizeObserver = new ResizeObserver((entries) => {
+			for (const entry of entries) {
+				const w = entry.contentBoxSize[0].inlineSize;
+				canvasRef.current.width = w;
+				canvasRef.current.height = (w * DEFAULT_HEIGHT) / DEFAULT_WIDTH;
+			}
+		});
+
+		resizeObserver.observe(wrapperRef.current);
+
+		return () => {
+			if (wrapperRef?.current) {
+				resizeObserver.unobserve(wrapperRef.current);
+			}
+		};
+	}, []);
 
 	useEffect(() => {
 		activeRef.current = active;
@@ -28,7 +48,12 @@ function Visualizer({ stream, active }) {
 
 		const dataArray = new Uint8Array(bufferLength);
 
-		canvasCtx.clearRect(0, 0, WIDTH, HEIGHT);
+		canvasCtx.clearRect(
+			0,
+			0,
+			canvasRef.current.width,
+			canvasRef.current.height,
+		);
 		let drawVisual;
 		function draw() {
 			if (!activeRef.current) {
@@ -39,17 +64,18 @@ function Visualizer({ stream, active }) {
 
 			analyser.getByteFrequencyData(dataArray);
 
-			canvasCtx.fillStyle = "rgba(0, 0, 0, 0.78)";
-			canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
+			const { width, height } = canvasRef.current;
+			canvasCtx.fillStyle = "rgba(0, 0, 0, 0.5)";
+			canvasCtx.fillRect(0, 0, width, height);
 
-			const barWidth = (WIDTH - bufferLength) / bufferLength;
+			const barWidth = (width - bufferLength) / bufferLength;
 			let barHeight;
 			let x = 0;
 			for (let i = 0; i < bufferLength; i++) {
 				barHeight = dataArray[i];
 
 				canvasCtx.fillStyle = "rgb(255, 189, 9)";
-				canvasCtx.fillRect(x, HEIGHT - barHeight, barWidth, barHeight);
+				canvasCtx.fillRect(x, height - barHeight, barWidth, barHeight);
 
 				x += barWidth + 1;
 			}
@@ -64,7 +90,15 @@ function Visualizer({ stream, active }) {
 		};
 	}, [stream]);
 
-	return <canvas ref={canvasRef} width={WIDTH} height={HEIGHT} />;
+	return (
+		<div ref={wrapperRef} className="visualizer__wrapper">
+			<canvas
+				ref={canvasRef}
+				width={DEFAULT_WIDTH}
+				height={DEFAULT_HEIGHT}
+			/>
+		</div>
+	);
 }
 
 export default Visualizer;
